49e9421399f22ab74473b6e34cc8b330d20566be
Abhinav Ranjan <abhinav.ranjan001@gmail.com>
2022-06-02 00:27:03 +0530

    Time: 14 ms (74.30%), Space: 136.4 MB (60.63%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,22 +1,26 @@
 class Solution {
-    List<List<String>> result = new ArrayList();
-    public List<List<String>> partition(String str) {
-        recur(str, 0, new ArrayList<String>(), new boolean[str.length()+1][str.length()+1]);
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList();
+        dfs(result, s, 0, new ArrayList(), new boolean[s.length()][s.length()]);
         return result;
     }
-    
-    public void recur(String str, int s, List<String> temp, boolean[][] dp){
-        if(s >= str.length()) result.add(new ArrayList<String>(temp));
-        else{
-            for(int i=s; i<str.length(); i++){
-                if(str.charAt(s) == str.charAt(i) && ((i-s) <= 2 || dp[s+1][i-1])){
+
+    public void dfs(List<List<String>> result, String str, int s, ArrayList<String> curr, boolean[][] dp) {
+        if (s >= str.length()) result.add(new ArrayList(curr));
+        else {
+            // point to note is that we are not running the loop till end pointer but to the str.length()
+            for (int i = s; i < str.length(); i++) {
+                // why dp[s+1][e-1] ==> because the string starting from 's' and ending at 'e' will be a palindrome if charAt(s) == charAt(e) and
+                // the substring starting at s+1 and ending at e-1 is also a substring
+                if (str.charAt(s) == str.charAt(i) && ((i-s) <= 2 || dp[s + 1][i - 1])) {
                     dp[s][i] = true;
-                    temp.add(str.substring(s, i+1));
-                    recur(str, i+1, temp, dp);
-                    temp.remove(temp.size()-1);
+                    curr.add(str.substring(s, i + 1));
+                    dfs(result, str, i + 1, curr, dp);
+                    curr.remove(curr.size() - 1);
                 }
             }
         }
     }
+
 
 }
