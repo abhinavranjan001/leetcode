5aea2c918697cb38bd0f8ce2328d989e7cf6ae74
Abhinav Ranjan <abhinav.ranjan001@gmail.com>
2022-06-05 23:24:44 +0530

    Time: 30 ms (54.75%), Space: 73.8 MB (36.27%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.java
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] gardenNoAdj(int n, int[][] edges) {
-    
         List<List<Integer>> graph = new ArrayList();
         
         for(int i=0; i<n; i++){
@@ -15,13 +14,19 @@ class Solution {
         int[] result = new int[n];
         
         for(int i=0; i<n; i++){
+            // max length is 5 (to make it 1 indexed), this could be d+1, where d ==> maximum degree of 
+            // any vertex in the graph
+            // we are getting the colors of our neighbours, and then we will fill the color in our index whatever
+            // is not taken by the neighbours
             boolean[] colors = new boolean[5];
             
             for(int nei: graph.get(i))
+                // colors is holding all the colors taken by the neighbours
                 colors[result[nei]] = true;
             
             for(int c=1; c<=4; c++)
                 if(!colors[c]){
+                    // pick the first color which is available
                     result[i] = c;
                     break;
                 } 
