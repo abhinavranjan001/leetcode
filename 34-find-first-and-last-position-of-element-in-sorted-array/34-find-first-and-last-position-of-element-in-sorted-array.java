93022bd4e7393349a9b3df191e66bafadd1b095a
Abhinav Ranjan <abhinav.ranjan001@gmail.com>
2022-06-23 00:15:31 +0530

    Time: 0 ms (100.00%), Space: 47 MB (63.57%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,18 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-
-        int binResult = binarySearch(nums, 0, nums.length - 1, target);
-        if (binResult == -1) return new int[]{-1, -1};
-
-        int l = 0, r = binResult;
-        int m = (l + r) / 2;
-
-        while (l <= r) {
-            m = (l + r) / 2;
-            if (nums[m] != target && nums[m + 1] == target) {
-                break;
-            } else if (nums[m] == target) r = m - 1;
-            else l = m + 1;
-        }
-
-        int left = m;
-
-        l = binResult;
-        r = nums.length - 1;
-        while (l <= r) {
-            m = (l + r) / 2;
-            if (nums[m] != target && nums[m - 1] == target) {
-                break;
-            } else if (nums[m] == target) l = m + 1;
-            else r = m - 1;
-        }
-
-        int right = m;
-        if(nums[left] != target) left++;
-        if(nums[right] != target) right--;
-        return new int[]{left, right};
-    }
-
-    public int binarySearch(int[] arr, int l, int r, int x) {
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (arr[m] == x) return m;
-            if (arr[m] < x) l = m + 1;
-            else r = m - 1;
-        }
-        return -1;
+        double left = target - 0.5, right = target + 0.5;
+        int l = bs(nums, left), r = bs(nums, right);
+        if(l == r) return new int[]{-1, -1};
+        return new int[]{l, r-1};
     }
     
+    public int bs(int[] nums, double target) {
+        int l = 0, h = nums.length-1;
+        while(l <= h){
+            int m = l + (h - l)/2;
+            if(target > nums[m]) l = m+1;
+            else h = m-1;
+        }
+        return l;
+    }
 }
